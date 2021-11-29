d67fd5f2b25be705808677733ab50f22d4e3204b
AlekseyOgorodnikov <alekseyogorodnikov@internet.ru>
2021-11-29 12:53:22 +0400

    handle in actions

--- ch3/server.go
+++ ch3/server.go
@@ -5,17 +5,34 @@ import (
 	"net/http"
 )
 
-type MyHandler struct{}
+type HelloHandler struct{}
+type WorldHandler struct{}
+type AllHandler struct{}
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
+
+func (h *AllHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Aleksey in new world!")
 }
 
 func main() {
-	handler := MyHandler{}
+	hello := HelloHandler{}
+	world := WorldHandler{}
+	all := AllHandler{}
+
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: &handler,
+		Addr: "127.0.0.1:8080",
 	}
+
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
+	http.Handle("/", &all)
+
 	server.ListenAndServe()
 }
